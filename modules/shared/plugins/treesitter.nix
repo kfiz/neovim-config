2e088930a5a87ea4acf3261a7154a549b5a27fbe
Doro Rose <doroerose@gmail.com>
2026-01-02 02:39:15 +0100

    disable treesitter refactor for now

--- modules/shared/plugins/treesitter.nix
+++ modules/shared/plugins/treesitter.nix
@@ -13,16 +13,16 @@ let
         folding.enable = true;
       };
 
-      treesitter-refactor = {
-        enable = true;
-        settings = {
-          highlight_definitions = {
-            enable = true;
-            # Set to false if you have an `updatetime` of ~100.
-            clear_on_cursor_move = false;
-          };
-        };
-      };
+      # treesitter-refactor = {
+      #   enable = true;
+      #   settings = {
+      #     highlight_definitions = {
+      #       enable = true;
+      #       # Set to false if you have an `updatetime` of ~100.
+      #       clear_on_cursor_move = false;
+      #     };
+      #   };
+      # };
 
       hmts.enable = true;
     };
