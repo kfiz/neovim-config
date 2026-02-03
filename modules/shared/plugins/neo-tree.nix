666efde02ad1aca05fbe4aeb62f450b797405162
Doro Rose <doroerose@gmail.com>
2026-02-03 04:03:27 +0100

    neo-tree: added some config

--- modules/shared/plugins/neo-tree.nix
+++ modules/shared/plugins/neo-tree.nix
@@ -14,11 +14,49 @@ let
 
       settings = {
         close_if_last_window = true;
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false;
+            hide_hidden = false;
+
+            never_show_by_pattern = [
+              ".direnv"
+              ".git"
+            ];
+
+            visible = true;
+          };
+        };
+
+        follow_current_file = {
+          enabled = true;
+          leave_dirs_open = true;
+        };
         window = {
           width = 30;
           auto_expand_width = true;
+          mappings = {
+            "<space>" = "none";
+            "x" = "cut_to_clipboard";
+            "c" = "copy_to_clipboard";
+            "o" = "toggle_node";
+            "l" = "focus_preview";
+            "e" = {
+              command = "toggle_preview";
+              config = {
+                use_float = true;
+              };
+            };
+            "<C-b>" = {
+              command = "scroll_preview";
+              config.direction = 10;
+            };
+            "<C-f>" = {
+              command = "scroll_preview";
+              config.direction = -10;
+            };
+          };
         };
-
       };
     };
   };
